89c8b3535b2a074058148b17322096717933ba8c
Bradley J. Main <bjmain@users.noreply.github.com>
2020-05-28 12:00:13 -0700

    Update maker_opts_update.py

--- maker_opts_update.py
+++ maker_opts_update.py
@@ -7,4 +7,4 @@ opts_files = glob.glob("chnk_*/maker_opts.ctl")
 for f in opts_files:
     filename = f.split("/")[0]
     new_chnk = "/home/bmain/otherpath/" + filename + "/" + filename + ".fa"
-    os.system("sed 's#/home/bmain/path/chnk1/chnk1.fa#%s#g' %s" % (new_chnk, f))
+    os.system("sed -i 's#/home/bmain/path/chnk1/chnk1.fa#%s#g' %s" % (new_chnk, f))
